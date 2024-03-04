b07ac5b22598415e6d891829a5139fac2869a9d8
mukeshkuiry <mukeshkk3162@gmail.com>
2024-03-05 00:35:40 +0530

    r added

--- server/main.go
+++ server/main.go
@@ -12,8 +12,15 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/peer", peer.HandlePeerConnection)
-	http.Handle("/", r)
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		// allow cross origin requests
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		w.Write([]byte("Hello, World!"))
+	}).Methods("GET")
 	log.Println("Serving at localhost:8000...")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
