24a15bdb962cd17c5a2744a0a51cd95a267a13b7
mukeshkuiry <mukeshkk3162@gmail.com>
2024-03-07 03:07:49 +0530

    h fix

--- client/app/utils/socketURL.ts
+++ client/app/utils/socketURL.ts
@@ -1,11 +1,11 @@
 let socketUrl: string;
 
 if (process.env.NODE_ENV === "production") {
-  socketUrl = "ws://localhost:8000/peer";
+  socketUrl = "wss://anycall.onrender.com/peer";
 } else if (process.env.NODE_ENV === "development") {
-  socketUrl = "ws://localhost:8000/peer";
+  socketUrl = "wss://anycall.onrender.com/peer";
 } else {
-  socketUrl = "ws://localhost:8000/peer";
+  socketUrl = "wss://anycall.onrender.com/peer";
 }
 
 export default socketUrl;
