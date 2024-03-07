3c22ee7ebd41cde8cc8d577f3fe30966b6cf2b90
mukeshkuiry <mukeshkk3162@gmail.com>
2024-03-07 17:53:49 +0530

    landing page for group chat

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,44 +2,21 @@
 /* eslint-disable @next/next/no-img-element */
 import Snowfall from "react-snowfall";
 import { motion } from "framer-motion";
-import JoinCard from "./components/JoinCard";
-import ChatBox from "./components/ChatBox";
-import { useSocket } from "./providers/Socket";
-import VideoStream from "./components/VideoStream";
-import { useEffect } from "react";
-import AnycallNavbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage";
 
 export default function Home() {
-  const { joined } = useSocket();
-
-  useEffect(() => {
-    document.title = joined ? "Chat Room" : "Join Chat Room";
-  }, [joined]);
-
   return (
     <>
       <div className="relative w-screen h-screen overflow-hidden bg-black">
         <Snowfall radius={[2, 5]} snowflakeCount={50} rotationSpeed={[1, 9]} />
-        {!joined ? (
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-            className="flex justify-center items-center h-full"
-          >
-            <JoinCard />
-          </motion.div>
-        ) : (
-          <div className="flex flex-col max-h-screen min-h-screen bg-black">
-            <AnycallNavbar />
-            <div className="flex lg:px-32 lg:mt-6 h-[calc(100vh-6rem)]">
-              <VideoStream />
-              <div className="hidden lg:inline w-2/3">
-                <ChatBox />
-              </div>
-            </div>
-          </div>
-        )}
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="flex justify-center items-center h-full"
+        >
+          <LandingPage />
+        </motion.div>
       </div>
     </>
   );
